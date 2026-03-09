18f924afdbe5b5292a167062fc3270e9be86fd16
Nikita Duzhenko <nickhellstr0m@protonmail.com>
2026-03-09 04:21:55 +0300

    Delete TR-BUTE directory

--- TR-BUTE/api/payment/create-link.js
+++
@@ -1,8 +0,0 @@
-/**
- * Payment Create Link - Entry Point
- *
- * Routes to the appropriate payment provider handler.
- * Currently uses T-Bank (EACQ) integration.
- */
-
-module.exports = require('./tbank/create-link');
--- TR-BUTE/api/payment/webhook.js
+++
@@ -1,8 +0,0 @@
-/**
- * Payment Webhook - Entry Point
- *
- * Routes to the appropriate payment provider webhook handler.
- * Currently uses T-Bank (EACQ) integration.
- */
-
-module.exports = require('./tbank/webhook');
